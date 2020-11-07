023836c7476f8933d153a240bb9a86a5268375a3
Dedaldino3D <dedaldinoantonio7@gmail.com>
2020-11-08 00:50:19 +0100

    Update routes in App

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,24 +1,31 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "../Home";
+import AboutUs from "../AboutUs";
+import Contact from "../Contact";
 import NotFound from "../404NotFound";
-import GlobalStyle from '../Styles'
-
+import GlobalStyle from "../Styles";
 
 export const App = (props) => {
   return (
     <>
-    <GlobalStyle/>
-    <Router>
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route path="*">
-          <NotFound />
-        </Route>
-      </Switch>
-    </Router>
+      <GlobalStyle />
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/info">
+            <Contact />
+          </Route>
+          <Route path="/about">
+            <AboutUs />
+          </Route>
+          <Route path="*">
+            <NotFound />
+          </Route>
+        </Switch>
+      </Router>
     </>
   );
 };
