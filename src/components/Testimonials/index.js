dbb23b3d116878256f0908af8ef1dc342f16079b
Dedaldino3D <dedaldinoantonio7@gmail.com>
2020-11-07 15:19:12 +0100

    Add Testimonials component

---
+++ src/components/Testimonials/index.js
@@ -0,0 +1,12 @@
+// import React from "react";
+// import { Carousel } from 'antd'
+
+// import { Testimonial } from "./styles";
+
+// export const Testimonials = (_props: any) => {
+//   return <Carousel autoplay>
+//     <Testimonial></Testimonial>
+//   </Carousel>;
+// };
+
+// export default Testimonials;
